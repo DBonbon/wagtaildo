b5986130e926cc13ad5a8c35d637058658a72c17
Yariv Friedman <yrfriedman@gmail.com>
2021-10-01 15:06:55 +0200

    allowed host

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = int(os.environ.get("DEBUG", default=1))
+DEBUG = True # int(os.environ.get("DEBUG", default=1))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
