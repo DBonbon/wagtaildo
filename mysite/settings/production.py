2a50d87bf31d7ff744894d147df290cc6f3a701d
Yariv Friedman <yrfriedman@gmail.com>
2021-10-01 19:53:34 +0200

    allowed host

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True # int(os.environ.get("DEBUG", default=1))
 SECRET_KEY = os.environ.get("SECRET_KEY")
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
 try:
     from .local import *
