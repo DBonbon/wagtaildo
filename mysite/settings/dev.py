f6bb00502c36262507f8973c516c2c937d129923
Yariv Friedman <yrfriedman@gmail.com>
2021-10-03 00:21:34 +0200

    allowed host * only

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -7,10 +7,11 @@ DEBUG = True
 SECRET_KEY = 'django-insecure-wt2^elcpvn+a-aa7exnt92ak#l%yozhx+!el^^g1o&z246vyyb'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['localhost', '157.230.20.7']
+ALLOWED_HOSTS = ['*'] # ['localhost', '157.230.20.7']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+INTERNAL_IPS = ("127.0.0.1", "172.17.0.1")
 
 try:
     from .local import *
--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,10 +1,10 @@
 from .base import *
 
-DEBUG = True # int(os.environ.get("DEBUG", default=1))
+DEBUG = int(os.environ.get("DEBUG", default=1))
 SECRET_KEY = os.environ.get("SECRET_KEY")
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
-try:
-    from .local import *
-except ImportError:
-    pass
+# try:
+#     from .local import *
+# except ImportError:
+#     pass
