9bbba42db03e3f857e6611a2391570c6c7d63b44
Yariv Friedman <yrfriedman@gmail.com>
2021-10-03 21:53:36 +0200

    allowed host without env file

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = True # int(os.environ.get("DEBUG", default=1))
-SECRET_KEY = "dbaa1_i7%*3r9-=z-+_mz4r-!qeed@(-a_r(g@k8jo8y3r27%m" # os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-wt2^elcpvn+a-aa7exnt92ak#l%yozhx+!el^^g1o&z246vyyb' # os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = ['*'] # os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # try:
