481616e222f99bb4909f22f82125b77458790134
Yariv Friedman <yrfriedman@gmail.com>
2021-10-03 23:05:28 +0200

    changing to staticfiles in static_root settings.py

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,8 +1,8 @@
 from .base import *
 
 DEBUG = True # int(os.environ.get("DEBUG", default=1))
-SECRET_KEY = 'django-insecure-wt2^elcpvn+a-aa7exnt92ak#l%yozhx+!el^^g1o&z246vyyb' # os.environ.get("SECRET_KEY")
-ALLOWED_HOSTS = ['*'] # os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+SECRET_KEY = 'django-insecure-wt2^elcpvn+a-aa7exnt92ak#l%yozhx+!el^^g1o&z246vyyb'
+ALLOWED_HOSTS = ['*']
 
 # try:
 #     from .local import *
@@ -11,6 +11,3 @@ ALLOWED_HOSTS = ['*'] # os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 from .base import *
 
-DEBUG = True
-SECRET_KEY = 'my_secret_key'
-ALLOWED_HOSTS = ['*']
